b43bed73be8a9aec5f55f00a66341275f42b6477
unknown <mouatazbillah97@gmail.com>
2021-09-09 19:34:21 +0100

    adding rep area text

--- new website/login.php
+++ new website/login.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>BusinessManMarket</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
@@ -9,46 +9,53 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
-<body>
 <style>
-    body{
-        background-color:darkgray;
+    #centry{
+        text-align:center;
+        
     }
+
 </style>
-<div class="jumbotron jumbotron-fluid text-center">
-  <h1>Welcome to <strong style="color:green">mysupermarket</strong></h1>
-  <p>Find quality Manufacturers, Suppliers, and suggest new products in future sells</p> 
+<body>
+
+<div class="jumbotron text-center ">
+  <h1><a class="text-danger">Welcome to</a> <strong style="color:green">BMMarket</strong></h1>
+  <h4 style="color:black;"> quality Manufacturers, Suppliers, and suggest new products in the future sells</h4> 
 </div>
 
 
-
-<form action = "" method = "post">
-    <div class="container-fluid">
+<form action = "" method = "post" >
+    <div class="container-fluid" >
         <div class="row justify-content-md-center">
         
             <div class="col-md-center">
 
-                <div class="col">
-                    <div class="p-3 border">
-                        <div class="input-group flex-nowrap">   
+                <div class="col" >
+                    <div class="p-3 border" >
+                        <div class="input-group flex-nowrap" >   
                             <span class="input-group-text" id="addon-wrapping">@</span>
                             <input type="text" id="myuse" name="email" placeholder="Username" aria-describedby="addon-wrapping">
                         </div>
                     </div>
                 </div>
 
-                <div class="col">
+                <div class="col" id="centry">
                     <div class="p-3 border">
                         <input type="text" id="mypass" name="password" placeholder="Password">
                     </div>
                 </div>
 
                 <div class="col">
-                    <div class="p-3 border">
+                    <div class="p-3 border" id="centry">
                         <button id="mylog" type="button" class="btn btn-dark" width="20px">Log In</button>
                         <button id="mysing" type="button" class="btn btn-dark" width="20px">Sing Up</button>
-                        <button id="myrep" type="button" class="btn btn-dark" width="20px">Report</button>
                     </div>
+                </div>
+            </div>
+            <div class="col-md-right">
+                <div class="col-2">
+                    <textarea name="textarea" id="reparea" cols="30" rows="10" placeholder="you can report an issue from here !!!"></textarea>
+                    <button id="myrep" type="button" class="btn btn-dark" width="20px" id="centry">Report</button>
                 </div>
             </div>
         </div>
